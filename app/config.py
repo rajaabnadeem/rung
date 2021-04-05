a5800238ac03266b0e28d0718688608f7c576cb1
Mohammed Nadeem <mrajaabnadeem@gmail.com>
2021-04-05 18:25:57 +0500

    pushing to heroku

--- app/config.py
+++ app/config.py
@@ -1,7 +1,24 @@
 import os
 
+
 class Config:
-  SECRET_KEY=os.environ.get('SECRET_KEY')
-  SQLALCHEMY_TRACK_MODIFICATIONS=False
-  SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-  SQLALCHEMY_ECHO=True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_ECHO = True
+
+
+module.exports = {
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: 'postgres',
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+    }
+}
